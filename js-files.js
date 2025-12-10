d50cdc3cbd1ba4f31d37e182476f30a53ed82e67
Elias Adams Aboagye <eliasaboagye@gmail.com>
2025-12-10 00:10:28 +0000

    added grid board for sketch

---
+++ js-files.js
@@ -0,0 +1,55 @@
+//start up grid size
+let n = 16;
+
+const gridSize = document.querySelector("input")
+const applyBtn = document.querySelector("button")
+const container = document.querySelector(".container");
+let items;
+// let color = "blue"
+// n = gridSize;
+
+function random(n){
+    return Math.floor(Math.random() * n + 1);
+}
+function createGrid(n){
+    for(let i = 0; i<n*n; i++){
+    const box = document.createElement("div");
+    box.className = "item"
+    container.appendChild(box);
+    }
+    items = document.querySelectorAll(".item");
+
+    items.forEach((item)=>{
+        item.style.flexBasis = `${100/n}%`;
+    })
+
+    items.forEach((item)=>{
+
+        item.addEventListener('mouseenter', (e)=>{
+            e.target.style.backgroundColor = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+            })
+    })
+}
+
+function applyGrid(){
+
+    n = gridSize.value;
+    if(isNaN(n) || n > 100 ){
+        return;
+    }
+    items.forEach((item) => {
+        item.style.display = "none"
+    })
+    createGrid(n);
+}
+createGrid(n);
+
+//apply grid when button is clicked
+applyBtn.addEventListener('click', applyGrid );
+//aply grid when enter key is pressed
+gridSize.addEventListener('keydown', (e)=>{
+    if(e.key = 'enter'){
+        applyGrid();
+    }
+})
+
